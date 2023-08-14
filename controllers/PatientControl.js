0b53803f4bfedf2f7f4ba164273b339c46a1e5eb
Alexei Palacios <sukesasuk2010@gmail.com>
2023-08-14 13:53:20 -0600

    [ADD] Patient Controller

---
+++ controllers/PatientControl.js
@@ -0,0 +1,93 @@
+const PatientModel = require("../models/PatientModel");
+const PatienModel = require("../models/PatientModel");
+
+module.exports = {
+    create: async (req, res) =>{
+
+    let body =  req.body;
+    let { name, age, sex, faceImage, curp, allergies, freqD, freqS, oxygen, signedletter, aditionalInfo, roadMap } = body;
+    let patient = new PatienModel({
+        name, 
+        age, 
+        sex,
+        faceImage,
+        curp,
+        allergies, 
+        freqD,
+        freqS,
+        oxygen,
+        signedletter,
+        aditionalInfo,
+        roadMap
+    });
+    patient.save((err, patientDB) =>{
+        if(err){
+            return res.status(400).json({
+                ok: false,
+                err,
+            });
+        }
+        res.json({
+            ok: true,
+            patient: patientDB
+        })
+    })
+},
+
+update: async (req, res) =>{
+    let body =  req.body;
+    let { name, age, sex, faceImage, curp, allergies, freqD, freqS, oxygen, signedletter, aditionalInfo, roadMap } = body;
+    let patient = new PatienModel({
+        name, 
+        age, 
+        sex,
+        faceImage,
+        curp,
+        allergies, 
+        freqD,
+        freqS,
+        oxygen,
+        signedletter,
+        aditionalInfo,
+        roadMap
+    });
+
+    await PatientModel.update({_id: req.body._id}, patient)
+    .then(patient =>{
+        if(!patient) res.json({ success: false, result: "Patient does not exist"});
+
+        reset.json(patient);
+    })
+    .catch(err =>{
+        res.json({ succes: false, result: err});
+    })
+},
+
+retrieve: async (req, res) =>{
+    await PatientModel.find()
+        .then(result =>{
+            if(!result) res.json({success: false, result: "No results found"});
+            
+
+            res.json({ succes: true, result: result});
+        })
+        .carch(err => res.json({success: false, rsult: err}));
+},
+
+retrieveOne: async (req, res) =>{
+    const { id } = req.params;
+    PatientModel.findById({_id: id})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}));
+},
+
+delete: async (req, res) =>{
+    await PatientModel.removeAllListeners({_id: req.body._id})
+        .then(result =>{
+            if(!result) res.json({success: false, result: "No user was found with the ID ${req.body.id}"});
+            res.json({ success: true, result});
+        })
+        .catch(err => res.json({ success: false, result: err}));
+    }
+
+}
