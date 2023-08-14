2c8c4cdf8677ba6be276c3af4730e1ada9a25a79
carlos <carlosweb@barcelostudio.com>
2023-08-14 17:08:05 -0600

    RELVED: SERVER:JS CHANGES

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 const express = require('express')
 const db = require('./db.js')
 const cors = require('cors');
 // const userRoutes = require('./routes/userRoutes');
-=======
 const express = require("express");
 const db = require("./db.js");
 const CosmoRoutes = require("./routes/CosmotologistRouter");
->>>>>>> 5fa5c5c (ADDED:CONTROLLER,VALIDATIONS,ROUTE FOR COSMOTOLOGIST AND SET TIMEZONE TO MEXICO CITY)
 
 const app = express();
 const port = 3000;
@@ -18,15 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-<<<<<<< HEAD
 // Routes 
 
 const PatientRoute = require('./routes/PatientRoute.js');
 
 app.use('/usc/patient', PatientRoute);
 
-=======
->>>>>>> 5fa5c5c (ADDED:CONTROLLER,VALIDATIONS,ROUTE FOR COSMOTOLOGIST AND SET TIMEZONE TO MEXICO CITY)
 //Middleware a futuro para la autenticacion
 //app.use(authMiddleware);
 
