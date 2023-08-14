c8bd79af6cf93a038bed8e9ce9177b2400daf900
carlos <carlosweb@barcelostudio.com>
2023-08-14 12:37:47 -0600

    ADDDED: COSMOTOLOGIST MODEL

---
+++ models/CosmotologistModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const CosmotolpgistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  full_lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  birthday: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  RegisteredAt: {
+    type: Date,
+    default: Date.now,
+  },
+  isOnline: {
+    type: Boolean,
+    default: false,
+  },
+});
+//location hace referencia a la clinica en la que esta la comotologa
+const Cosmotologist = mongoose.model(Cosmotologist, CosmotolpgistSchema);
+
+module.exports = Cosmotologist;
