dbd24cc354728b3d3b7a24456c5eeeae2952eb3c
carlos <carlosweb@barcelostudio.com>
2023-08-24 18:16:00 -0600

    FIXED POPULATE

--- controllers/HistoryController.js
+++ controllers/HistoryController.js
@@ -1,49 +1,57 @@
 const HistoryModel = require("../models/HistoryModel");
 const TreatmentModel = require("../models/TreatmentModel");
+const mongoose = require("mongoose");
 
 module.exports = {
+  create: async (req, res) => {
+    let history = new HistoryModel({
+      patient: req.body.patient,
+      treatment: req.body.treatment,
+      fecha: req.body.fecha,
+    });
 
-    create: async(req, res) =>{
+    await history
+      .save()
+      .then((result) => {
+        res.json({ success: true, result: result });
+      })
+      .catch((err) => {
+        res.json({ success: false, result: err });
+      });
+  },
 
-        let history = new HistoryModel({
-            patient: req.body.patient,
-            treatment: req.body.treatment,
-            fecha: req.body.fecha
-        })
+  retrieve: async (req, res) => {
+    await HistoryModel.find()
+      .populate("patient")
+      .populate("treatment")
+      .then((result) => {
+        res.json({ success: true, result: result });
+      })
+      .catch((err) => {
+        res.json({ success: false, result: err });
+      });
+  },
 
-        await history.save()
-            .then(result =>{
-                res.json({ success: true, result: result})
-            })
-            .catch(err => {
-                res.json({ success: false, result: err})
-            })
-    },
-    
-    retrieve: async(req, res) =>{
-        await HistoryModel.find()
+  retrieveOne: async (req, res) => {
+    try {
+      const history = await HistoryModel.findById({ _id: req.body._id })
         .populate("patient")
-        .populate("treatment")
-            .then(result =>{
-                res.json({ success: true, result: result})
-            })
-            .catch(err =>{
-                res.json({ success: false, result: err})
-            })
-    },
-
-    retrieveOne: async (req, res) =>{
-       await HistoryModel.findOne({patient: req.body.patient})
-            .populate("patient")
-            .populate("treatment")
-                .then((data) => {
-                    TreatmentModel.findOne({_id: data.treatment._id})
-                    .populate("product")
-                        .then(result => res.json( {success: true, result: data}))
-                })
-                .catch((error) => res.json(error));
-    },
-    
-
-
-}
+        .populate("treatment");
+  
+      if (!history) {
+        return res.json({ success: false, message: "Historia no encontrada" });
+      }
+  
+      // Tratamiento con datos poblados
+      const treatment = history.treatment;
+  
+      // Ahora, poblamos el campo 'product' en 'treatment'
+      const populatedTreatment = await TreatmentModel.populate(treatment, { path: "product" });
+  
+      res.json({ success: true, result: { history, treatment: populatedTreatment } });
+    } catch (error) {
+      res.json(error);
+    }
+  }
+  ,
+};
