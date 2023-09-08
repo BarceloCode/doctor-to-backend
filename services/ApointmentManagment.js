c71010cdf7ce71cb245b3bba2e0ef27cfbc33ac2
carlos ezequiel gonzalez tisareño <carlosweb@barcelostudio.com>
2023-09-07 19:15:31 -0600

    MODIFIED: ApointmentManagment -> show BU of Cosme and populate clinic

--- services/ApointmentManagment.js
+++ services/ApointmentManagment.js
@@ -1,22 +1,60 @@
 const apoitmentSch = require("../models/ApointmentModel");
 const businessUnitSchema = require("../models/BusinessUnitModel");
+const cosmetologistSchema = require("../models/CosmetologistModel");
 
 async function findCosmetologistByTreatment(req) {
   try {
     //pasarle el array de los tratamientos.
-    const BusinessUnit = await businessUnitSchema.find({
-      treatment: { $in: req.body.treatment },
-    }).select("_id name");
+    const BusinessUnit = await businessUnitSchema
+      .find({
+        treatment: { $in: req.body.treatment },
+      })
+      .select("_id");
 
-    let flag = BusinessUnit.length;
-    if (flag === 0) {
+    let lenght = BusinessUnit.length;
+    if (lenght === 0) {
       return {
-        message: "Error cant find BusinessUnit with treatments with IDs: '" + req.body.treatment + "'",
-        status: 400
+        message:
+          "Cant find BusinessUnit that can do this treatment with IDs: '" +
+          req.body.treatment +
+          "'",
+        status: 400,
       };
     }
     if (BusinessUnit) {
-      return { message: "Success", BusinessUnit: BusinessUnit, status: 200 };
+      const ids = [];
+
+      BusinessUnit.forEach((element) => {
+        const idString = element._id.toString();
+        ids.push(idString);
+      });
+      const Cosmetologist = await cosmetologistSchema
+        .find({
+          businessUnit: { $in: ids },
+        })
+        .select("_id worktime workdays name full_lastname businessUnit")
+        .populate({
+          path: "businessUnit",
+          select: { name: 1, clinic: 1 },
+          populate: { path: "clinic" },
+        });
+      lenght = Cosmetologist.length;
+      if (lenght === 0) {
+        return {
+          message:
+            "Error cant find Cosmetologist with BusinesUnit with IDs: '" +
+            ids +
+            "'",
+          status: 400,
+        };
+      }
+      if (Cosmetologist) {
+        return {
+          message: "Cosmetologists that can do this treatment(s):" + ids.length,
+          Cosmetologist: Cosmetologist,
+          status: 200,
+        };
+      }
     }
   } catch (error) {
     return { message: error.message, status: 400 };
