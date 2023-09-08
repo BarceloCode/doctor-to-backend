42cfd461f0e126ee958bdc252a7f82b02fddba08
carlos ezequiel gonzalez tisareño <carlosweb@barcelostudio.com>
2023-09-07 18:10:39 -0600

    [MODIFIED]: ApointmentManagmentController, Service, Validations - DONE SELECT TREATMENTS AND FIND BU WITH THOSE TREATMENTS

--- services/ApointmentManagment.js
+++ services/ApointmentManagment.js
@@ -1,12 +1,26 @@
 const apoitmentSch = require("../models/ApointmentModel");
 const businessUnitSchema = require("../models/BusinessUnitModel");
 
-
 async function findCosmetologistByTreatment(req) {
-  //pasarle los id de los tratamientos.
-  const BusinessUnit = await businessUnitSchema.find({treatment: req.body.treatment});
-  
-  console.log(BusinessUnit);
+  try {
+    //pasarle el array de los tratamientos.
+    const BusinessUnit = await businessUnitSchema.find({
+      treatment: { $in: req.body.treatment },
+    }).select("_id name");
+
+    let flag = BusinessUnit.length;
+    if (flag === 0) {
+      return {
+        message: "Error cant find BusinessUnit with treatments with IDs: '" + req.body.treatment + "'",
+        status: 400
+      };
+    }
+    if (BusinessUnit) {
+      return { message: "Success", BusinessUnit: BusinessUnit, status: 200 };
+    }
+  } catch (error) {
+    return { message: error.message, status: 400 };
+  }
 }
 
 module.exports = {
